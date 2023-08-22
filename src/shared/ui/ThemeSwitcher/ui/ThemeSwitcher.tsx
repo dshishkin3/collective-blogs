ea801dcd94a8ec440b2688977d4630e07742b498
Dmitry Shishkin <79608355+ihopeyoucanfly@users.noreply.github.com>
2023-08-22 17:29:52 +0300

    aa

--- src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -1,10 +1,12 @@
 import { Theme, useTheme } from "app/prodivers/themeProvider";
-import React, { FC } from "react";
+import { FC } from "react";
 import { classNames } from "shared/lib/classNames/classNames";
 import { Button, ThemeButton } from "shared/ui/Button/Button";
 
 import LightIcon from "shared/assets/icons/theme-light.svg";
 import DarkIcon from "shared/assets/icons/theme-dark.svg";
+
+import styles from "./ThemeSwitcher.module.scss";
 
 interface ThemeSwitherProps {
   className?: string;
